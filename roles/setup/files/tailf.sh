2b92846e96525f57e4d33e33783c46595e08a9d0
kowshikvajrala <kowshik@encapsulate.xyz>
2024-10-14 15:24:48 +0530

    Update tailf script

--- roles/setup/files/tailf.sh
+++ roles/setup/files/tailf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-tail -f "$@" | sed -r 's/\x1B\[[0-9;]*[mK]//g' | sed \
-    -e 's/.*\(INFO\|INF\).*/\x1B[32m&\x1B[39m/I' \
-    -e 's/.*\(ERROR\|ERR\).*/\x1B[31m&\x1B[39m/I' \
-    -e 's/.*\(DEBUG\|DBG\).*/\x1B[36m&\x1B[39m/I' \
-    -e 's/.*\(WARN\|WRN\).*/\x1B[33m&\x1B[39m/I' \
-    -e 's/.*\(TRACE\|TRC\).*/\x1B[35m&\x1B[39m/I'
+tail -f "$@" | sed -E '
+    s/\x1B\[[0-9;]*m//g;
+    /INFO|INF/I {s/^/\x1B[32m/; s/$/\x1B[39m/}
+    /ERROR|ERR/I {s/^/\x1B[31m/; s/$/\x1B[39m/}
+    /DEBUG|DBG/I {s/^/\x1B[36m/; s/$/\x1B[39m/}
+    /WARN|WRN/I {s/^/\x1B[33m/; s/$/\x1B[39m/}
+    /TRACE|TRC/I {s/^/\x1B[35m/; s/$/\x1B[39m/}
+'
