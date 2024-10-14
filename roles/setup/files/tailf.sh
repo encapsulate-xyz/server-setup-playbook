8c09938bd99e8fd691afaf1ad3cede01156ec058
Aditya Verma <aditya.verma.manit@gmail.com>
2024-10-14 17:08:51 +0530

    update tail.sh

--- roles/setup/files/tailf.sh
+++ roles/setup/files/tailf.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
+# Start tailing the file(s) and apply sed to colorize log levels
 tail -f "$@" | sed -E '
+    # Remove any existing ANSI color codes (to avoid color code stacking)
     s/\x1B\[[0-9;]*m//g;
-    /INFO|INF/I {s/^/\x1B[32m/; s/$/\x1B[39m/}
-    /ERROR|ERR/I {s/^/\x1B[31m/; s/$/\x1B[39m/}
-    /DEBUG|DBG/I {s/^/\x1B[36m/; s/$/\x1B[39m/}
-    /WARN|WRN/I {s/^/\x1B[33m/; s/$/\x1B[39m/}
-    /TRACE|TRC/I {s/^/\x1B[35m/; s/$/\x1B[39m/}
+
+    # Colorize INFO (green)
+    /INFO|INF/I {
+        s/^/\x1B[32m/;    # Add green color to the beginning
+        s/$/\x1B[39m/     # Reset color at the end
+    }
+
+    # Colorize ERROR (red)
+    /ERROR|ERR/I {
+        s/^/\x1B[31m/;    # Add red color
+        s/$/\x1B[39m/     # Reset color
+    }
+
+    # Colorize DEBUG (cyan)
+    /DEBUG|DBG/I {
+        s/^/\x1B[36m/;    # Add cyan color
+        s/$/\x1B[39m/     # Reset color
+    }
+
+    # Colorize WARNING (yellow)
+    /WARN|WRN/I {
+        s/^/\x1B[33m/;    # Add yellow color
+        s/$/\x1B[39m/     # Reset color
+    }
+
+    # Colorize TRACE (magenta)
+    /TRACE|TRC/I {
+        s/^/\x1B[35m/;    # Add magenta color
+        s/$/\x1B[39m/     # Reset color
+    }
 '
